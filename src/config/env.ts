373d13b8ead9363cae478a5d9cda6221d65fbe1b
Kader <kaderkaaya7@gmail.com>
2026-01-08 01:15:58 +0300

    database connection added

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,36 +1,33 @@
-// import { Sequelize } from "sequelize";
-// import { fileURLToPath } from "url";
-// import path from "path";
-// import dotenv from "dotenv";
+import { Sequelize } from "sequelize";
+import dotenv from 'dotenv';
 
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = path.dirname(__filename);
-// dotenv.config({
-//     path: path.resolve(__dirname, '../../.env'),
-// });
+dotenv.config({
+});
 
-// //burda env dosyası src dısında oldugu ıcın path ile yolunu belirttık
-// const DB_DIALECT = process.env.DB_DIALECT;
-// const DB_HOST = process.env.DB_HOST;
-// const DB_NAME = process.env.DB_NAME;
-// const DB_USER = process.env.DB_USER;
-// const DB_PASSWORD = process.env.DB_PASSWORD;
 
-// const sequelize = new Sequelize(
-//     DB_NAME,
-//     DB_USER,
-//     DB_PASSWORD,
-//     {
-//         host: DB_HOST,
-//         dialect: DB_DIALECT,
-//         logging: false,
-//     }
-// );
+const DB_DIALECT = process.env.DB_DIALECT;
+const DB_HOST = process.env.DB_HOST;
+const DB_NAME = process.env.DB_NAME;
+const DB_USER = process.env.DB_USER;
+const DB_PASSWORD = process.env.DB_PASSWORD;
 
-// try {
-//     await sequelize.authenticate();
-//     console.log("Database connection has been established successfully.");
-// } catch (error) {
-//     console.error("Unable to connect to the database:", error);
-// }
-// export default sequelize;
+const sequelize = new Sequelize(
+    DB_NAME!,
+    DB_USER!,
+    DB_PASSWORD!,
+    {
+        host: DB_HOST,
+        dialect: DB_DIALECT as 'mysql',
+        logging: false,
+    }
+);
+
+export const connectDatabase = async () => {
+try {
+    await sequelize.authenticate();
+    console.log("Database connection has been established successfully.");
+} catch (error) {
+    console.error("Unable to connect to the database:", error);
+}
+};
+export default sequelize;
--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,10 @@
 import express from 'express';
+import { connectDatabase } from './config/env.js';
+import dotenv from 'dotenv';
 
+dotenv.config();
+
+await connectDatabase();
 const app = express();
 app.use(express.json());
 
@@ -7,6 +12,6 @@ app.get('/health', (_, res) => {
     res.json({ ok: true });
 });
 
-app.listen(3000, () => {
-    console.log('Server running on http://localhost:3000');
+app.listen(process.env.PORT, () => {
+    console.log(`Server running on http://localhost:${process.env.PORT}`);
 });
